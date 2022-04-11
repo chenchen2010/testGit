0a589cb6f16736023b3b88d2a58365f151780c67
chenchen <2629733393@qq.com>
2022-04-11 16:54:13 +0800

    HelloWorld

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * All rights Reserved, Designed By www.XXXX.com
+ *
+ * @author 陈晨
+ * @version V1.0.0
+ * @projectName testGit
+ * @title HelloWorld
+ * @package PACKAGE_NAME
+ * @description
+ * @date 2022/4/11 16:53
+ * @copyright 2022 www.XXXXX.com
+ * 注意 本内容仅限于 南京星源动力信息技术有限公司，禁止外泄以及用于其他的商业
+ */
+public class HelloWorld {
+}
