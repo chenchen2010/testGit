4217bdc6780577031e5fcacb2bd73bf53ae0ef87
chenchen <2629733393@qq.com>
2022-04-11 17:11:53 +0800

    这是一个测试类

---
+++ src/main/java/Test.java
@@ -0,0 +1,18 @@
+/**
+ * All rights Reserved, Designed By www.XXXX.com
+ *
+ * @author 陈晨
+ * @version V1.0.0
+ * @projectName testGit
+ * @title Test
+ * @package PACKAGE_NAME
+ * @description
+ * @date 2022/4/11 17:11
+ * @copyright 2022 www.XXXXX.com
+ * 注意 本内容仅限于 南京星源动力信息技术有限公司，禁止外泄以及用于其他的商业
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("这是一个测试类");
+    }
+}
